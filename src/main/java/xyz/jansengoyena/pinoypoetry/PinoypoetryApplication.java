078b5a1f17b20de870388acb3a2c4226d1963149
Jansen <jansen.goyena@yahoo.com>
2018-09-06 15:22:07 +0800

    initial spring boot commit

---
+++ src/main/java/xyz/jansengoyena/pinoypoetry/PinoypoetryApplication.java
@@ -0,0 +1,12 @@
+package xyz.jansengoyena.pinoypoetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinoypoetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinoypoetryApplication.class, args);
+	}
+}
