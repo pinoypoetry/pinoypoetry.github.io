1048740e4e430d5cc1f3267af131f7e5b843045b
Jansen <jansen.goyena@yahoo.com>
2018-09-07 13:21:03 +0800

    updated codedeploy scripts to deploy automatically to tomcat

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-echo "Hello World!"
 
+CONTEXT_PATH='ROOT'
+# Remove Artifcats if available
+if [[ -f $CATALINA_HOME/webapps/$CONTEXT_PATH.war ]]; then
+    rm $CATALINA_HOME/webapps/$CONTEXT_PATH.war
+fi
+if [[ -d $CATALINA_HOME/webapps/$CONTEXT_PATH ]]; then
+    rm -rfv $CATALINA_HOME/webapps/$CONTEXT_PATH
+fi
+
+cp /appl/pinoypoetry-1.0.0.war $CATALINA_HOME/webapps/$CONTEXT_PATH.war
+
+service tomcat8 start
